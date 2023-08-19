30cc9af68648eae2d682ff730cdc81fde763573b
Gabriel Tidy <gabriel@bbd.co.za>
2023-08-19 18:45:50 +0200

    fix: enabled passing of data to postDrink endpoint

--- api/app.ts
+++ api/app.ts
@@ -1,12 +1,19 @@
 import express, { Application, Request, Response } from 'express'
+import * as dotenv from 'dotenv';
 import userRouter from './src/controllers/user'
 import { DBController } from './src/controllers/DBController'
+import cors from 'cors'
+
+dotenv.config();
 
 const databaseController = new DBController();
 
 const app: Application = express()
 
 const port: number = 8080
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/user', userRouter);
 
@@ -15,20 +22,27 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.get('/getDrinks', async (req: Request, res: Response) => {
-    const response = await databaseController.executeQuery('SELECT * FROM DopYourDopDB.DrinkHistory');
+    const response = await databaseController.executeQuery(`SELECT * FROM ${process.env.DATABASE_NAME}.${process.env.TABLE_NAME}`);
     res.send(response);
 });
 
-app.get('/postDrink', async (req: Request, res: Response) => {
+app.post('/postDrink', async (req: Request, res: Response) => {
 
-    const username = 'test_user';
-    const drink = 'beer';
-    const quantity = 1.5;
+    const data = {
+        username: req?.body?.username,
+        drink: req?.body?.drink,
+        quantity: parseFloat(req?.body?.quantity),
+    };
+
+    if (!data?.username || !data.drink || !req?.body?.quantity || isNaN(data?.quantity)) {
+        res.status(400).send('Invalid query parameters.');
+        return;
+    }
 
     const response = await databaseController.executeWrite(
-        username,
-        drink,
-        quantity
+        data.username,
+        data.drink,
+        data.quantity
     );
 
     res.send(response);
